e0b3d0625dab03e071d67c25ed8981dae05d7685
Grace <gracelauren909@gmail.com>
2015-04-24 01:54:43 -0700

    EX9

---
+++ ex9.py
@@ -0,0 +1,21 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" #\n is a line break
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+test
+test
+test
+test
+test
+test
+test
+test
+test
+""" #three double quotes is like an unlimited? varchar
