b68d01414d41a6a7ddc60fd44e1af920df9a1bde
Grace <gracelauren909@gmail.com>
2015-04-24 12:43:40 -0700

    ex12 prompting people

---
+++ ex12.py
@@ -0,0 +1,13 @@
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
+
+
+#study drills
+#In Terminal where you normally run python to run your scripts, type pydoc raw_input. Read what it says. If you're on Windows try python -m pydoc raw_input instead.
+#Get out of pydoc by typing q to quit.
+#Look online for what the pydoc command does.
+#Use pydoc to also read about open, file, os, and sys. It's alright if you do not understand those; just read through and take notes about interesting things.
