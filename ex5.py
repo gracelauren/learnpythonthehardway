79cabfa61bc0c6e3f506263b9da2877a103e3a51
Grace <gracelauren909@gmail.com>
2015-04-23 17:03:04 -0700

    ex 5

---
+++ ex5.py
@@ -0,0 +1,17 @@
+name = 'Grace Lauren'
+age = 99 #this is a lie
+height = 61 #this is in inches
+weight = 110 #lbs
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Dark Brown'
+
+print "Let's talk about %s." % name
+print "She's %d inches tall." % height
+print "She's %d pounds heavy." % weight
+print "Actually that's not too heavy."
+print "She's got %s eyes and %s hair." %(eyes, hair)
+print "Her teeth are usually %s depending on the coffee." % teeth
+
+print "If I add %d, %d, and %d I get %d." % (
+    age, height, weight, age + height + weight)
