002ea7865b1f65bceb6c6ba36272143581b83e58
Grace <gracelauren909@gmail.com>
2015-04-27 16:44:54 -0700

    data types

---
+++ data_types.py
@@ -0,0 +1,9 @@
+TYPE	DESCRIPTION.	EXAMPLE
+True	True boolean value.	True or False == True
+False	False boolean value.	False and True == False
+None	Represents "nothing" or "no value".	x = None
+strings	Stores textual information.	x = "hello"
+numbers	Stores integers.	i = 100
+floats	Stores decimals.	i = 10.389
+lists	Stores a list of things.	j = [1,2,3,4]
+dicts	Stores a key=value mapping of things.	e = {'x': 1, 'y': 2}
